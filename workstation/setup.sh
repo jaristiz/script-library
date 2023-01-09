0f56bbef00e670d4df2849bb018c07a8e712d953
Jhonathan E Aristizabal <j_aristi@hotmail.com>
2023-01-08 22:27:54 -0500

    added snapd config

--- workstation/setup.sh
+++ workstation/setup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-sudo apt-get update
+sudo apt-get update -y
+sudo apt-get -y upgrade snapd
 
 brew update
 brew install direnv
 brew install jq
 brew install azure-cli
 brew install terraform 
+
+sudo snap install bw
